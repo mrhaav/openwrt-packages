17ae5baeb0b1135c3d0de0092bc7efdca4b9e925
mrhaav <62175065+mrhaav@users.noreply.github.com>
2022-12-04 13:10:54 +0100

    Update me909s_sms_r.sh

--- me909s/files/usr/bin/me909s_sms_r.sh
+++ me909s/files/usr/bin/me909s_sms_r.sh
@@ -5,7 +5,8 @@
 # SMS rules
 DEV=$(uci get network.wwan.ttyDEV)
 
-updateFirewall() {
+# examlpe of a firewall rule change
+rule1() {
 	action=$(echo "$1" | sed -n '1p' | sed -e 's/[\r\n]//g')
 	IPaddress=$(echo "$1" | sed -n '2p' | sed -e 's/[\r\n]//g')
 	logger -t mem909s_r $action $IPaddress
@@ -45,112 +46,10 @@ updateFirewall() {
 }
 
 
-bagaAlarm() {
-	action=$(echo "$1" | sed -n '1p' | sed -e 's/[\r\n]//g')
-	if [ ${action:0:1} = 'B' ]
-	then
-		mqttMessage='Cancel'
-		alarmCode=${action:1:4}
-		postTopic='Alarm'
-	elif [ ${action:0:1} = '1' ]
-	then
-		mqttMessage='Raised'
-		alarmCode=${action:0:4}
-		postTopic='Alarm'
-	else
-		postTopic='Status'
-		alarmCode=$(echo $action | awk -F '=' '{print $1}')
-		mqttMessage=$(echo $action | awk -F '=' '{print $2}')
-		mqttMessage='{"parameterName":"'$alarmCode'","Value":'$mqttMessage',"dateTime":"'$dateTime'"}'
-	fi
-	case $alarmCode in
-		1111 )
-			if [ $mqttMessage == 'Raised' ]
-			then
-				mqttMessage='Minor'
-			fi
-			mqttMessage='{"alarmName":"Flocculants","alarmStatus":"'$mqttMessage'","dateTime":"'$dateTime'"}'
-			;;
-		1114 )
-			if [ $mqttMessage == 'Raised' ]
-			then
-				mqttMessage='Minor'
-			fi
-			mqttMessage='{"alarmName":"LowLevelSludgeSeparator","alarmStatus":"'$mqttMessage'","dateTime":"'$dateTime'"}'
-			;;
-		1124 )
-			if [ $mqttMessage == 'Raised' ]
-			then
-				mqttMessage='Major'
-			fi
-			mqttMessage='{"alarmName":"HighLevelSludgeSeparator","alarmStatus":"'$mqttMessage'","dateTime":"'$dateTime'"}'
-			;;
-		1224 )
-			if [ $mqttMessage == 'Raised' ]
-			then
-				mqttMessage='Major'
-			fi
-			mqttMessage='{"alarmName":"LongPumptime","alarmStatus":"'$mqttMessage'","dateTime":"'$dateTime'"}'
-			;;
-		1264 )
-			if [ $mqttMessage == 'Raised' ]
-			then
-				mqttMessage='Major'
-			fi
-			mqttMessage='{"alarmName":"MaxPumptime","alarmStatus":"'$mqttMessage'","dateTime":"'$dateTime'"}'
-			;;
-		1300 )
-			if [ $mqttMessage == 'Raised' ]
-			then
-				mqttMessage='Varning'
-			fi
-			mqttMessage='{"alarmName":"Powerfailure","alarmStatus":"'$mqttMessage'","dateTime":"'$dateTime'"}'
-			;;
-		1359 )
-			mqttMessage='{"alarmName":"GSMmodulOK","alarmStatus":"Notice","dateTime":"'$dateTime'"}' ;;
-		1389 )
-			if [ $mqttMessage == 'Raised' ]
-			then
-				mqttMessage='Major'
-			fi
-			mqttMessage='{"alarmName":"FuseF1","alarmStatus":"'$mqttMessage'","dateTime":"'$dateTime'"}'
-			;;
-		1444 )
-			mqttMessage='{"alarmName":"Calibration","alarmStatus":"Notice","dateTime":"'$dateTime'"}' ;;
-		1459 )
-			if [ $mqttMessage == 'Raised' ]
-			then
-				mqttMessage='Minor'
-			fi
-			mqttMessage='{"alarmName":"LowTemperature","alarmaStatus":"'$mqttMessage'","dateTime":"'$dateTime'"}'
-			;;
-		1534 )
-			if [ $mqttMessage == 'Raised' ]
-			then
-				mqttMessage='Minor'
-			fi
-			mqttMessage='{"alarmName":"LevelSensorError","alarmaStatus":"'$mqttMessage'","dateTime":"'$dateTime'"}'
-			;;
-		* )
-			if [ $postTopic == 'Alarm' ]
-			then
-				mqttMessage='{"alarmName":"'$alarmCode'","alarmStatus":"'$mqttMessage'","dateTime":"'$dateTime'"}'
-			fi
-			;;
-	esac
-	
-	mosquitto_pub -h ip.ginstmark.se -t Arholma/Baga/$postTopic -m $mqttMessage
-	
-	if [ $postTopic == 'Alarm' ]
-	then
-		BagaSMS="+46708331512"$'\n'"Baga "$(echo $mqttMessage | jq -r .alarmName)$'\n'$(echo $mqttMessage | jq -r .alarmStatus)' '$(echo $mqttMessage | jq -r .dateTime)
-		/urs/bin/me909s_sms_t "$BagaSMS"
-	fi
 
-	if [ $postTopic == 'Status' ]
-	then
-		/etc/henrik/bagadata.sh $mqttMessage
-	fi
+rule2() {
+	action=$(echo "$1" | sed -n '1p' | sed -e 's/[\r\n]//g')
+# add your own rules
 
 }
 
@@ -182,14 +81,14 @@ atOut=$(COMMAND="AT+CMGD=$SMSindex" gcom -d "$DEV" -s /etc/gcom/run_at.gcom)
 
 # Allowed receivers
 case $sender in
-	'+46708331512' )
-		updateFirewall "$SMStext"
+	'+46708123456' )
+		rule1 "$SMStext"
 		;;
-	'+46701426725' )
-		bagaAlarm "$SMStext"
+	'+46701987654' )
+		rule2 "$SMStext"
 		;;
 	* )
-		failedSMS="+46708331512"$'\n'"from: ${sender} ${dateTime}"$'\n'${SMStext}
+		failedSMS="+46708123456"$'\n'"from: ${sender} ${dateTime}"$'\n'${SMStext}
 		/usr/bin/me909s_sms_t.sh "$failedSMS"
 		;;
 esac
